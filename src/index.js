8f5c8711697f0b25954652bbb061df311271ef09
Bersi Vanessa Zuñiga Luque <vanessa.zuniga@finkargo.com>
2022-06-07 18:50:36 -0500

    se cambia configuracion

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
-  authDomain:process.env.REACT_APP_AUTHDOMAIN ,
-  projectId: process.env.REACT_APP_PROYECTID ,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROYECTID,
   storageBucket: process.env.REACT_APP_STORAGEBICKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID ,
-  appId: process.env.REACT_APP_APPID
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
